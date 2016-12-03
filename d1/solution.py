ba0710a4b5d0d79cf62b0ce2ea6ca556c476770e
Matthew Tse <mtse9779@gmail.com>
2016-12-03 16:57:59 -0500

    Update working solution2 and problem test case

--- d1/solution.py
+++ d1/solution.py
@@ -39,6 +39,23 @@ def p1_solution(instr):
 
     return coord.dist_from_origin()
 
+def p2_solution(instr):
+    instr = instr.split(', ')
+    visited = set()
+    coord = coordinates()
+    dir_to_axis = {'N':('y',1),'S':('y',-1),'E':('x',1),'W':('x',-1)}
+
+    for line in instr:
+        direction,steps = line[0],int(line[1:])
+        coord.turn(direction)
+        for i in range(steps):
+            axis,increment = dir_to_axis[coord.dir]
+            coord.move(axis,increment)
+            if (coord.x,coord.y) not in visited:
+                visited.add((coord.x,coord.y))
+            else:
+                return coord.dist_from_origin()
+    return coord.dist_from_origin()
 
 def test_answer():
     assert p1_solution('R2, L3') == 5
@@ -52,3 +69,5 @@ def test_answer():
     assert p2_solution('R8, R5, R4, R8') == 4
     assert p2_solution('R8, R4, R5, R8') == 3
     assert p2_solution('R1, R1, R1, R1') == 0
+    #below is the numerical p2_solution to the question
+    assert p2_solution('L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2') == 163
