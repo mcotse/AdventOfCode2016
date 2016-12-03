07ab741e753b5a46ac43187214c96877c953ea9a
Matthew Tse <mtse9779@gmail.com>
2016-12-03 14:35:36 -0500

    Update solution to account for bigger numpads Keep track of numpad state in coordinates class
    Update in_bounds() condition to check for 0

--- d2/solution.py
+++ d2/solution.py
@@ -1,38 +1,40 @@
 import pytest
 
 class coordinates:
-    def __init__(self):
-        self.x = 1
-        self.y = 1
+    def __init__(self,x=1,y=1,numpad=[[1,2,3],[4,5,6],[7,8,9]]):
+        self.x = x
+        self.y = y
+        self.numpad = numpad
+        self.size = len(numpad)
 
     def move(self,d,step):
-        # print 'd:',d
-        # print 'step:',step
         if d == 'U' or d == 'D':
-            if self.in_bounds(self.y+step):
+            if self.in_bounds(self.y+step,self.y):
                 self.y += step
         elif d == 'L' or d == 'R':
-            if self.in_bounds(self.x+step):
+            if self.in_bounds(self.x+step,self.x):
                 self.x += step
-        # print 'coords:', self.x, self.y
 
-    def in_bounds(self,index):
-        if -1 < index < 3:
-            return True
-        else: return False
+    def in_bounds(self,index,axis):
+        if (-1 < index < self.size):
+            if axis == self.y and self.numpad[index][self.x]:
+                return True
+            elif axis == self.x and self.numpad[self.y][index]:
+                return True
+        return False
+
+    def get_curr(self):
+        return self.numpad[self.y][self.x]
 
 def solution1(instr):
     code = []
     coord = coordinates()
-    numpad = [[1,2,3],[4,5,6],[7,8,9]]
     move_map = {'U':-1,'D':1,'L':-1,'R':1}
-    # dir_map = {'U':coord.y,'D':coord.y,'L':coord.x,'R':coord.x}
 
-    # x,y = 1,1
     for instr_row in instr.split('\n'):
         for move in instr_row:
             coord.move(move,move_map[move])
-        code.append(numpad[coord.y][coord.x])
+        code.append(coord.get_curr())
 
     return int(''.join(str(x) for x in code))
 
